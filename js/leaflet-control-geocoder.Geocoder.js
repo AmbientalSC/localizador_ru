be63e8c7eac7f3c8fbcf930dbce3046eccebfc72
AlyssonKrombauer <alysson@ambiental.sc>
2024-02-26 11:41:56 -0300

    ajuste de zoom e beatify nos JS

--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,7 +10,7 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Search...',
+			placeholder: 'Pesquisar...',
 			errorMessage: 'Nothing found.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
