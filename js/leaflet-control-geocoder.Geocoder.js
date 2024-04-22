5a4167f080d5cbc191cb7f4c2d99350dc43ad406
AlyssonKrombauer <alysson@ambiental.sc>
2024-04-22 16:03:47 -0300

    Update leaflet-control-geocoder.Geocoder.js

--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,7 +10,7 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Search...',
+			placeholder: 'Pesquise RUA/CEP + N° + Cidade',
 			errorMessage: 'Nothing found.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
