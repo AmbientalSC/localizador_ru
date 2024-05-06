c7f08890d2f42d00ef73d931daeb40efd951ac73
AlyssonKrombauer <alysson@ambiental.sc>
2024-05-06 10:24:30 -0300

    ajuste label

--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,8 +10,8 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Search...',
-			errorMessage: 'Nothing found.',
+			placeholder: 'Pesquise RUA/CEP + N° + Cidade',
+			errorMessage: 'Endereço não encontrado.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
 			defaultMarkGeocode: true
