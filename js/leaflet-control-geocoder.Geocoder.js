3120984517f6b488e645fbd42a5724eba7df7e32
AlyssonKrombauer <alysson@ambiental.sc>
2024-08-26 09:25:14 -0300

    inclusão de jaraguá do sul

--- js/leaflet-control-geocoder.Geocoder.js
+++ js/leaflet-control-geocoder.Geocoder.js
@@ -10,7 +10,7 @@ module.exports = {
 			collapsed: true,
 			expand: 'touch', // options: touch, click, anythingelse
 			position: 'topright',
-			placeholder: 'Pesquise RUA/CEP + N° + Cidade...',
+			placeholder: 'Search...',
 			errorMessage: 'Nothing found.',
 			suggestMinLength: 3,
 			suggestTimeout: 250,
